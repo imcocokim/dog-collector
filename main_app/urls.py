e4922f1f57c8f36ce9cfe92e74981ace31e25e30
imcoco <imcoco@gmail.com>
2022-08-04 17:43:14 -0400

    dog index html

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('about/', views.about, name='about'),
+  path('about/', views.home, name='about'),
+  path('dogs/', views.dogs_index, name='dogs_index'),
 ]

6aaa2a6846a4c6bf25877be03295cb6784a11600
imcoco <imcoco@gmail.com>
2022-08-07 15:46:49 -0400

    add signup and login

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
-  path('about/', views.home, name='about'),
+  path('', views.Home.as_view(), name='home'),
+  path('about/', views.about, name='about'),
   path('dogs/', views.dogs_index, name='dogs_index'),
   path('dogs/<int:dog_id>', views.dogs_detail, name='dogs_detail'),
   path('dogs/create/', views.DogCreate.as_view(), name='dogs_create'),
@@ -17,5 +17,7 @@ urlpatterns = [
   path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
   path('dogs/<int:dog_id>/add_feeding/', views.add_feeding, name='add_feeding'),
   path('dogs/<int:dog_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
+  path('accounts/signup/', views.signup, name='signup')
+
 
 ]

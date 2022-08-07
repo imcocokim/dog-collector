809fbcf4d704f3c66fd1fe9da0b4dc4daf34b65f
imcoco <imcoco@gmail.com>
2022-08-07 11:30:29 -0400

    feeding details

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),  
+  ('L', 'Lunch'),
+  ('S', 'Snack'),
+  ('D', 'Dinner')
+)
+
 # Create your models here.
 
 
@@ -14,5 +21,20 @@ class Dog(models.Model):
     return self.name
 
   def get_absolute_url(self):
-      return reverse("dog_detail", kwargs={"dog_id": self.id})
+      return reverse("dogs_detail", kwargs={"dog_id": self.id})
+
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+
+  dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.get_meal_display()} on {self.date}'
   
+  class Meta:
+    ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
+from .forms import FeedingForm
 
 
 # Create your views here.
@@ -16,7 +17,11 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
-  return render(request, 'dogs/detail.html', { 'dog': dog })
+  feeding_form = FeedingForm()
+  return render(request, 'dogs/detail.html', { 
+    'dog': dog,
+    'feeding_form': feeding_form
+  })
 
 class DogCreate(CreateView):
   model = Dog
@@ -30,3 +35,11 @@ class DogUpdate(UpdateView):
 class DogDelete(DeleteView):
   model = Dog
   success_url ='/dogs/'
+
+def add_feeding(request, dog_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.dog_id = dog_id
+    new_feeding.save()
+  return redirect('dogs_detail', dog_id=dog_id)
